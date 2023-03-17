0e99a291220077604783699b6b7fa72c732012e4
Mike Rohner <mrohner94@hotmail.com>
2023-03-17 11:54:47 -0400

    tests

--- src/config.rs
+++ src/config.rs
@@ -27,7 +27,7 @@ impl TryFrom<Opts> for Config {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Operation {
     Print(Option<String>),
     Add(String, String),
@@ -96,5 +96,79 @@ fn get_pwd(pwd: Option<PathBuf>) -> Result<PathBuf> {
         return Ok(pwd);
     }
 
-    return Ok(std::env::current_dir().context("errored getting currend_dir")?);
+    return Ok(std::env::current_dir().context("errored getting current_dir")?);
+}
+
+#[cfg(test)]
+mod test {
+    use anyhow::Result;
+
+    use crate::{opts::Opts, config::Operation};
+
+    use super::Config;
+
+
+
+    #[test]
+    fn test_print_all() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![],
+            pwd: None,
+            config: None,
+        }.try_into()?;
+       
+
+        assert_eq!(opts.operation, Operation::Print(None));
+        return Ok(());
+    }
+
+    #[test]
+    fn test_print_key() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![
+                String::from("foo")
+            ],
+            pwd: None,
+            config: None,
+        }.try_into()?;
+
+        assert_eq!(opts.operation, Operation::Print(Some(String::from("foo"))));
+        return Ok(());
+    }
+
+    #[test]
+    fn test_add_key_value() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![
+                String::from("add"),
+                String::from("foo"),
+                String::from("bar"),
+            ],
+            pwd: None,
+            config: None,
+        }.try_into()?;
+
+        assert_eq!(opts.operation, Operation::Add(
+                String::from("foo"),
+                String::from("bar")));
+
+        return Ok(());
+    }
+
+    #[test]
+    fn test_remove_key() -> Result<()> {
+        let opts: Config = Opts {
+            args: vec![
+                String::from("rm"),
+                String::from("foo"),
+            ],
+            pwd: None,
+            config: None,
+        }.try_into()?;
+
+        assert_eq!(opts.operation, Operation::Remove(
+                String::from("foo")));
+
+        return Ok(());
+    }
 }

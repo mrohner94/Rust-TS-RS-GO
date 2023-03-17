09ad424969e6e71bf51fdbb59abd6adb9272e42a
Mike Rohner <mrohner94@hotmail.com>
2023-03-16 23:01:52 -0400

    Config setup

--- src/bin/projector.rs
+++ src/bin/projector.rs
@@ -1,6 +1,10 @@
 use clap::Parser;
+use Rust_TS_RS_GO:: {opts::Opts, config::Config};
+use anyhow::Result;
+fn main() -> Result<()> {
+    // let opts = Rust_TS_RS_GO::opts::Opts::parse();
+    let opts: Config = Opts::parse().try_into()?;
+    println!("{:?}", opts);
 
-fn main() {
-    let opts = Rust_TS_RS_GO::opts::Opts::parse();
-    println!("{:?}", opts)
+    return Ok(())
 }
